1482c78d7ff47cff182c4b8e4489bb6239bf936a
ReyADayer <reyaddayer@gmail.com>
2020-08-15 07:50:13 +0900

    :heavy_plus_sign: GroupTypeを作成

---
+++ src/main/kotlin/net/atlantis/jinrocraft/model/GroupType.kt
@@ -0,0 +1,7 @@
+package net.atlantis.jinrocraft.model
+
+enum class GroupType(val key: String, val jpName: String) {
+    CITIZENS("Citizens", "市民陣営"),
+    WEREWOLVES("Werewolves", "人狼陣営"),
+    FOXES("Foxes", "妖狐陣営");
+}
