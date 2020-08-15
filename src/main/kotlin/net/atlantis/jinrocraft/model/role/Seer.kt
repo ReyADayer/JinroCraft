f7a40cc622a79e7858ff318e0e71cc07d739aa9b
ReyADayer <reyaddayer@gmail.com>
2020-08-15 15:55:34 +0900

    :bug: 経験値の消費を追加

--- src/main/kotlin/net/atlantis/jinrocraft/model/role/Seer.kt
+++ src/main/kotlin/net/atlantis/jinrocraft/model/role/Seer.kt
@@ -12,6 +12,7 @@ class Seer : Role() {
     override fun onClickedEntity(player: Player, targetEntity: Entity) {
         if (targetEntity is Player && player.level >= 10 && player.equipment?.itemInMainHand?.type == Material.SHEARS) {
             val targetRoleType = RoleService().getRole(targetEntity)
+            player.level -= 10
             when (targetRoleType) {
                 RoleType.WEREWOLF -> {
                     player.sendMessage("${ChatColor.RED}${targetEntity.name}は ${ChatColor.BOLD}人狼 ${ChatColor.RESET}でした")
