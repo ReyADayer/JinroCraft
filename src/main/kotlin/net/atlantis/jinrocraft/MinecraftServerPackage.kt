850d4df107e09ceb39dd6c98dc6ab39b2e1e5ee1
ReyADayer <reyaddayer@gmail.com>
2020-08-20 15:59:19 +0900

    :art: リファクタリング

--- src/main/kotlin/net/atlantis/jinrocraft/MinecraftServerPackage.kt
+++ src/main/kotlin/net/atlantis/jinrocraft/MinecraftServerPackage.kt
@@ -6,7 +6,7 @@ object MinecraftServerPackage {
     @Throws(ClassNotFoundException::class)
     fun getClass(className: String): Class<*> {
         val version = Bukkit.getServer().javaClass.`package`.name.replace("org.bukkit.craftbukkit.", "")
-        val path = "net.minecraft.server." + version
+        val path = "net.minecraft.server.$version"
         return Class.forName("$path.$className")
     }
 }
