b95ee5595047e4ab37fcabac8b186d028aee5a7c
ReyADayer <reyaddayer@gmail.com>
2020-08-26 20:35:30 +0900

    :white_check_mark: TimeTypeのテストを作成

---
+++ src/test/java/net/atlantis/jinrocraft/model/time/TimeTypeTest.kt
@@ -0,0 +1,28 @@
+package net.atlantis.jinrocraft.model.time
+
+import io.mockk.every
+import io.mockk.mockk
+import org.bukkit.World
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class TimeTypeTest {
+
+    @Test
+    fun findByWorld() {
+        Assertions.assertEquals(TimeType.MOONING, TimeType.findByWorld(getWorld(0)))
+        Assertions.assertEquals(TimeType.MOONING, TimeType.findByWorld(getWorld(5000)))
+        Assertions.assertEquals(TimeType.MOONING, TimeType.findByWorld(getWorld(11999)))
+        Assertions.assertEquals(TimeType.EVENING, TimeType.findByWorld(getWorld(12000)))
+        Assertions.assertEquals(TimeType.EVENING, TimeType.findByWorld(getWorld(13999)))
+        Assertions.assertEquals(TimeType.NIGHT, TimeType.findByWorld(getWorld(14000)))
+        Assertions.assertEquals(TimeType.NIGHT, TimeType.findByWorld(getWorld(23999)))
+        Assertions.assertNull(TimeType.findByWorld(getWorld(24000)))
+    }
+
+    private fun getWorld(time: Long): World {
+        val world = mockk<World>()
+        every { world.time } returns time
+        return world
+    }
+}
