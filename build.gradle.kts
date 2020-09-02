5efc7b908da9018d31c9a3207b09c27d6e5424b3
ReyADayer <reyaddayer@gmail.com>
2020-09-02 14:25:43 +0900

    :arrow_up: ProtocolLibを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,14 @@ repositories {
     mavenCentral()
     maven(Dependencies.Spigot.repository)
     maven(Dependencies.SonaType.repository)
+    maven(Dependencies.ProtocolLib.repository)
 }
 
 dependencies {
     compile(Dependencies.Spigot.api)
     compileOnly(Dependencies.Spigot.annotations)
     kapt(Dependencies.Spigot.annotations)
+    compile(Dependencies.ProtocolLib.core)
     compile(Dependencies.Kotlin.stdlib)
     compile(Dependencies.Kotlin.reflect)
     compile(Dependencies.Rx.java)
